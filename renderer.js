2743c6391646399412e295e7050c69185e46b01c
Andrew MacDonald <macdonald.andrew@gmail.com>
2018-03-22 21:54:44 -0700

    Test the screen_api_id param.

--- renderer.js
+++ renderer.js
@@ -16,7 +16,7 @@ var captureScreens = () => {
   screenInfo.textContent = ''
   $('#screenVideos').empty()
   desktopCapturer.getSources({
-    types: ['screen'],
+    types: ['window', 'screen'],
     thumbnailSize: {
       width: 320,
       height: 240
@@ -29,34 +29,35 @@ var captureScreens = () => {
       canvas.width += size.width
       canvas.height = Math.max(canvas.height, size.height)
 
-      screenNames.textContent += `${source.id} (${source.name}), `
+      screenNames.textContent += `${source.id} ${source.screen_api_id} (${source.name}), `
       const dx = canvas.width - size.width
       img.onload = () => ctx.drawImage(img, dx, 0)
       img.src = source.thumbnail.toDataURL()
-
-      navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: 'desktop',
-            chromeMediaSourceId: source.id,
-            minWidth: 640,
-            maxWidth: 640,
-            minHeight: 320,
-            maxHeight: 320
+      if (source.id.includes('screen')) {
+        navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: {
+            mandatory: {
+              chromeMediaSource: 'desktop',
+              chromeMediaSourceId: source.id,
+              minWidth: 640,
+              maxWidth: 640,
+              minHeight: 320,
+              maxHeight: 320
+            }
           }
-        }
-      })
-      .then((stream) => {
-        const screenVideos = document.getElementById('screenVideos')
-        const video = document.createElement('video')
-        screenVideos.appendChild(video)
-        video.srcObject = stream
-        video.onloadedmetadata = () => {
-          video.play()
-        }
-      })
-      .catch((error) => console.error(error))
+        })
+        .then((stream) => {
+          const screenVideos = document.getElementById('screenVideos')
+          const video = document.createElement('video')
+          screenVideos.appendChild(video)
+          video.srcObject = stream
+          video.onloadedmetadata = () => {
+            video.play()
+          }
+        })
+        .catch((error) => console.error(error))
+      }
     })
     const displays = electron.screen.getAllDisplays()
     displays.forEach((display) => {
